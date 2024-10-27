e15e86b3902d72514531a680282a322b05824f0b
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-10-27 05:22:43 -0300

    icone

--- Auto.py
+++ Auto.py
@@ -128,7 +128,7 @@ def criar_interface_grafica():
     root.configure(bg='#2E4053')  # Fundo azul escuro
 
     # Adiciona o ícone do programa
-    root.iconbitmap('Icone_AutoPes_002.ico')
+    root.iconbitmap('22287dragon_98813.png')
 
     style = ttk.Style()
     style.configure("TLabel", font=("Helvetica", 12), background='#2E4053', foreground='white')
