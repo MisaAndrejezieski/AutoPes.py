60df907841f93efd067fc063603db1555c4804ff
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-11-04 16:17:03 -0300

    Up

--- Auto.py
+++ Auto.py
@@ -31,7 +31,7 @@ temas_en = [
 perguntas_en = [
     "What is {tema}?", "What are the latest news in {tema}?",
     "How does {tema} impact society?", "What are the main challenges in {tema}?",
-    "Who are the leading experts in {tema}?", "how to make money with {tema}",
+    "Who are the leading experts in {tema}?", "How to make money with {tema}",
 ]
 
 # Função para gerar uma lista de pesquisas aleatórias sobre um tema
@@ -106,18 +106,18 @@ def executar_automacao(num_temas=6, num_perguntas=6):
             if abrir_edge():
                 for _ in range(num_temas):
                     tema = random.choice(temas_en)
-                    pesquisas = gerar_pesquisas_sobre_tema(tema, num_perguntas)
+                    pesquisas = gerar_pesquisas_sobre_tema( tema, num_perguntas)
                     for pesquisa in pesquisas:
                         realizar_pesquisa(pesquisa)
-                limpar_dados_navegacao()
-                fechar_navegador ()
-                messagebox.showinfo("Finalizado", "Automação concluída com sucesso.")
+                    limpar_dados_navegacao()
+                fechar_navegador()
+                logging.info("Automação concluída com sucesso.")
             else:
-                messagebox.showerror("Erro", "Não foi possível abrir o navegador Edge.")
+                logging.error("Falha ao abrir o navegador.")
         else:
-            messagebox.showerror("Erro", "Não foi possível verificar a conectividade com a internet.")
+            logging.error("Falha na verificação de conectividade com a internet.")
     except Exception as e:
-        messagebox.showerror("Erro", f"Ocorreu um erro durante a automação: {e}")
+        logging.error(f"Erro ao executar a automação: {e}")
 
 # Função para rodar a automação em segundo plano
 def iniciar_automacao_bg(num_temas, num_perguntas):
@@ -145,18 +145,18 @@ def iniciar_interface():
     style.map('TButton', background=[('active', '#56b6c2')])
     style.configure('Red.TButton', background='#f44336', foreground='#ffffff', font=('Helvetica', 12, 'bold'))
     style.map('Red.TButton', background=[('active', '#d32f2f')])
-    style.configure('TLabel', background='#282c34', foreground='#61afef', font=('Helvetica', 10))
-    style.configure('TEntry', font=('Helvetica', 10))
+    style.configure('TLabel', background='#282c34', foreground='#61afef', font=('Helvetica', 12))
+    style.configure('TEntry', font=('Helvetica', 12), padding=5)
 
     # Elementos da interface
-    ttk.Label(root, text="Número de Temas:", style='TLabel').grid(row=0, column=0, padx=20, pady=20, sticky='ew')
-    num_temas = ttk.Entry(root)
-    num_temas.grid(row=0, column=1, padx=20, pady=20, sticky='ew')
+    ttk.Label(root, text="Número de Temas:", style='TLabel').pack(pady=10)
+    num_temas = ttk.Entry(root, width=20)
+    num_temas.pack(pady=5)
     num_temas.insert(0, "1")
 
-    ttk.Label(root, text="Número de Perguntas por Tema:", style='TLabel').grid(row=1, column=0, padx=20, pady=20, sticky='ew')
-    num_perguntas = ttk.Entry(root)
-    num_perguntas.grid(row=1, column=1, padx=20, pady=20, sticky='ew')
+    ttk.Label(root, text="Número de Perguntas por Tema:", style='TLabel').pack(pady=10)
+    num_perguntas = ttk.Entry(root , width=20)
+    num_perguntas.pack(pady=5)
     num_perguntas.insert(0, "1")
 
     def iniciar_automacao():
@@ -168,23 +168,15 @@ def iniciar_interface():
         except ValueError:
             messagebox.showerror("Erro", "Por favor, insira valores numéricos válidos.")
 
-    ttk.Button(root, text="Iniciar Automação", command=iniciar_automacao, style='TButton').grid(row=2, column=0, columnspan=2, pady=20, sticky='ew')
+    ttk.Button(root, text="Iniciar Automação", command=iniciar_automacao, style='TButton').pack(pady=10)
 
     # Adicionar botão para fechar o programa
-    ttk.Button(root, text="Fechar Programa", command=root.quit, style='Red.TButton').grid(row=3, column=0, columnspan=2, pady=10, sticky='ew')
+    ttk.Button(root, text="Fechar Programa", command=root.quit, style='Red.TButton').pack(pady=10)
 
     # Adicionar imagem .png na interface
     img = PhotoImage(file=image_path)
     img_label = tk.Label(root, image=img)
-    img_label.grid(row=4, column=0, columnspan=2, pady=10, sticky='nsew')
-
-    root.grid_columnconfigure(0, weight=1)
-    root.grid_columnconfigure(1, weight=1)
-    root.grid_rowconfigure(0, weight=1)
-    root.grid_rowconfigure(1, weight=1)
-    root.grid_rowconfigure(2, weight=1)
-    root.grid_rowconfigure(3, weight=1)
-    root.grid_rowconfigure(4, weight=1)
+    img_label.pack(pady=10)
 
     root.mainloop()
 
